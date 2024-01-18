855013cdd6217f2053eeaed73de885254732820a
LucasAdao <l.adao@outlook.com>
2024-01-18 20:09:49 -0300

    Update

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using conversor_de_moedas.models;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace conversor_de_moedas
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void botaoCalcular1_Click(object sender, RoutedEventArgs e)
+        {
+            double dinheiroBox1 = Convert.ToDouble(textBox1.Text); 
+            Main();
+            async Task Main()
+            {
+                
+    
+                string apiUrl = $"https://api.invertexto.com/v1/currency/USD_BRL?token=6163|IFtvPOFE3EeFpXPBEt8quai2giB5mTAD"
+    ;
+
+                using (HttpClient client = new HttpClient())
+                {
+                    try
+                    {
+                        HttpResponseMessage response = await client.GetAsync(apiUrl);
+
+                        if (response.IsSuccessStatusCode)
+                        {
+                            string responseBody = await response.Content.ReadAsStringAsync();
+
+                            Money money = JsonConvert.DeserializeObject<Money>(responseBody);
+                            DateTime dateTime = DateTimeOffset.FromUnixTimeSeconds(money.currency.TimeStamp).DateTime;
+                            MessageBox.Show($"Neste exato momento:{dateTime} o dolar está valendo {Math.Round(money.currency.CurrencyValue,2)}R$");
+
+                            double valorDoDinheiro = money.currency.CurrencyValue * dinheiroBox1;
+                            textBoxResultado1.Text = $"Valor em Reais: {Math.Round(valorDoDinheiro,2)}R$";
+
+
+                        }
+                        else
+                        {
+                            
+                            MessageBox.Show("");
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"Erro: {ex.Message}");
+                    }
+                }
+            }
+            }
+
+       
+        }
+    }
---
+++ models/Currency.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace conversor_de_moedas.models
+{
+    public class Currency
+    {
+        [JsonProperty("price")]
+        public double CurrencyValue { get; set; }
+        [JsonProperty("timestamp")]
+        public int TimeStamp { get; set; }
+     
+    }
+}
